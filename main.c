49c7dbc6a944d7ef2487a07e8f198e14abb5e739
mich160 <mihal160@gmail.com>
2014-11-15 16:54:51 +0100

    logi

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <time.h>
 
 //deklaracje funkcji
 int synchronize(char * source, char * target);
@@ -16,7 +17,9 @@ int checkFile(char * source, char * target);
 int main(int argc, char* args[]) {
 
     pid_t pid, sid;
-
+	time_t currentTime;
+	struct time* localTime;
+	
     pid = fork();
     if (pid < 0) {
             exit(EXIT_FAILURE);
@@ -26,6 +29,10 @@ int main(int argc, char* args[]) {
     }
     umask(0);
     sid = setsid();
+	currentTime = time(NULL):
+	localTime = localTime(&currentTime);
+	openlog("SyncDaemon",LOG_PID);
+	
     if (sid < 0) {
             exit(EXIT_FAILURE);
     }
